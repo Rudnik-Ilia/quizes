8a6474ab91c168b077f97b0d10b10ca012837c65
ilia.rudnik <nov-gruz@mail.ru>
2022-08-04 15:37:26 +0300

    final ver

--- c/ws1/ws1.c
+++ c/ws1/ws1.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 
-void ChangeString(char *word, int len_of_word)/* takes word and its len. ti convert to hex-format*/
+/*void ChangeString(char *word, int len_of_word) takes word and its len. ti convert to hex-format
 
 {
     int i = 0;
        
     for(i = 0; i<len_of_word; i++)
     {
-        printf("0x%x\n",*(word+i));
+        printf("0x%x",*(word+i));
     }   
-}
+} */
 
 float PowNumber(int n)/*exponent to by n*/
 {
@@ -17,7 +17,7 @@ float PowNumber(int n)/*exponent to by n*/
 	float number = 10;
 	if (n < 0)
 	 {
-	     n*=-1;
+	     n *= -1;
 	     number = 0.1;
 	 }
 	 
@@ -34,7 +34,7 @@ int SwapNumber(int x)/*reverse a given number*/
 	int result = 0;
 	while(x)
 	{
-    		result = result*10+x%10;
+    		result = result * 10 + x % 10;
     		x /= 10;
 	}
 	return result;
@@ -53,17 +53,20 @@ int main()
 {
 	int a = 10;
 	int b = 20;
-
 	
-	char word[] = "\"Hello word!\"";	
-        ChangeString(word,17);
-    	printf("\n%f\n",PowNumber(5));
-    	printf("\n%f\n",PowNumber(-3));
-    	printf("%d\n",SwapNumber(12340));
+	char str[] = {0x22, 0x48, 0x65, 0x6C, 0x6C, 0x6F, 0x20, 0x77, 0x6F, 0x72, 0x6C, 0x64, 0x21, 0x22, 0};
+	printf("%s\n", str);
+	
+	//char word[] = "\"Hello word!\"";	
+        
+    	//printf("\n%f\n",PowNumber(5));
+    	//printf("\n%f\n",PowNumber(-3));
+    	//printf("%d\n",SwapNumber(12340));
        
     
-    	Swap(&a,&b);
-    
+    	Swap(&a, &b);
+    	
+    	
     
     
     return 0;
