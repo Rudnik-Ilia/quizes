4e0f20086d0ddfcc0d0c82561ebef49e2863d64f
ilia.rudnik <nov-gruz@mail.ru>
2022-08-04 12:07:54 +0300

    after_changes

--- c/ws1/ws1.c
+++ c/ws1/ws1.c
@@ -6,7 +6,7 @@ void ChangeString(char *word, int len_of_word)
        
     for(i = 0; i<len_of_word; i++)
     {
-        printf("0x%x",*(word+i));
+        printf("0x%x\n",*(word+i));
     }   
 }
 
@@ -47,48 +47,22 @@ void Swap(int *x,int *y)
 	*y = tmp;
 }
 
-long l = 8;
-long *foo(){ return &l; }
 
 int main()
 {
 
-	*(foo()) = 5;
-	printf("%ld\n", l);
-    char word[] = "\"Hello word!\"";	
-    // ChangeString(word,17);
-    //printf("\n%f",PowNumber(5));
-    //printf("\n%f",PowNumber(-3));
-    //printf("%d",SwapNumber(12340));
-    int a = 10;
-    int b = 20;
+	
+	char word[] = "\"Hello word!\"";	
+        ChangeString(word,17);
+    	printf("\n%f\n",PowNumber(5));
+    	printf("\n%f\n",PowNumber(-3));
+    	printf("%d\n",SwapNumber(12340));
+        int a = 10;
+        int b = 20;
     
-    //Swap(&a,&b);
+    	Swap(&a,&b);
     
-    //printf("%d - %d", a,b);
     
-    int x = 20;
-    int y = 35;
-    
-    x = x++ + y++;
-    
-    //printf("%d - %d\n", x,y);
-    y = ++x + ++y;
-    
-    //printf("%d - %d", x,y);
-    
-    int i = 2;
-    //int d;int c;
-    
-    //d = i++;
-    //printf("%d\n", d);
-    //i = 0;
-    //c = ++i; 
-    //printf("%d", c);
-    
-    //int arr[] = {0};
-    //arr[i] = i++;
-    //printf("%d", arr[i]);
     
     return 0;
 }
