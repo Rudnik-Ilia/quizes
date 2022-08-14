da86dcf7cb8ac6f5125ac02081d969e5628dbe5e
ilia.rudnik <nov-gruz@mail.ru>
2022-08-14 10:12:18 +0300

    start next one

---
+++ c/ws4/main.c
@@ -0,0 +1,31 @@
+/**********************
+* Title : Worksheet 3 
+* Author: Ilia Rudnik
+* Reviewer: 
+* Date : 13/08/2022
+***********************/
+
+#include <ncurses.h>
+#include<stdio.h> /* printf*/
+#include<stdlib.h> /* Malloc */
+#include<assert.h> /* Assert */
+#include"func.c" /* my functions */
+#include"tests.c" /* my tests*/
+
+#include"ws.h"
+
+
+
+int main()
+{
+
+
+
+
+
+
+
+
+
+return 0;
+}
