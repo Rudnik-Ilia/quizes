5f4f2b491a047251399f1fb12e31f0c00ba03d37
ilia.rudnik <nov-gruz@mail.ru>
2022-08-14 17:07:54 +0300

    second task

--- c/ws4/main.c
+++ c/ws4/main.c
@@ -5,35 +5,78 @@
 * Date : 16/08/2022
 ***********************/
 
-#include <ncurses.h>
+#include <ncurses.h> /*getchar compile with flag -lncurses!!!!!!!!!!!!!!!!!!!!!!!!*/
 #include<stdio.h> /* printf*/
 #include<stdlib.h> /* Malloc */
 #include<assert.h> /* Assert */
 #include"func.c" /* my functions */
 #include"tests.c" /* my tests*/
-
 #include"ws4.h"
 
 
-void hello()
+typedef void (*p_func)();
+
+
+int PrintAT_IF()
 {
-	printf("Hello");
+	char x;
+	system("stty -icanon -echo");
+	
+	do
+	{
+		printf("Insert the char: \n");
+		x = getchar();
+		
+		if('t' == x) printf("T-pressed!\n");
+		if('a' == x) printf("A-pressed!\n");
+		else printf("Try again!\n");
+		
+	}
+	while(27 != x);
+	system("stty icanon echo");
+	return 0;
 }
 
-int he(int x, int y)
+int PrintAT_SWITCH()
 {
-	return x*y;
+	char x;
+	system("stty -icanon -echo");
+	printf("Insert the char: \n");
+	x = getchar();
+	
+	do
+	{
+		printf("Insert the char: \n");
+		x = getchar();
+		switch(x)
+		{
+			case 't': 
+				printf("T-pressed!\n");
+				break;
+			case 'a': 
+				printf("a-pressed!\n");
+				break;
+			default:
+				printf("Try again!\n");
+			
+			
+		}
+	}
+	while(27 != x);
+	system("stty icanon echo");
+	return 0;
 }
 
 
 int main()
 {
-
-	int (*message) (int, int);
-     
-    message=he;
-    printf("%d\n",message(3,5));
-
+	
+	
+	
+	
+	
+	
+	
 
 
 
