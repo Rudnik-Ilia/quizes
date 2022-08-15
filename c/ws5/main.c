f288cdaff8b3ae40d1c3cc81925c35613d4b64f7
ilia.rudnik <nov-gruz@mail.ru>
2022-08-15 17:48:35 +0300

    second draft

--- c/ws5/main.c
+++ c/ws5/main.c
@@ -9,12 +9,17 @@
 #include<stdio.h> /* printf*/
 #include<stdlib.h> /* Malloc */
 #include<assert.h> /* Assert */
+#include<string.h>
+
 #include"ws5.h"
 
 
 typedef void (*p_func)(int);
 
+typedef int (*p_tostrcmp)(const char*, const char*);
 
+
+/*simple struct*/
 typedef struct
 {
     int number;
@@ -22,13 +27,26 @@ typedef struct
     
 } myStruct;
 
+/* big another struct*/
+typedef struct
+{
+    char *command;
+    p_tostrcmp Checking;
+    p_func Action;
+    
+} mySuperStruct;
+
 
 void Print(int number)
 {
 	printf("%d\n", number);
 }
 
-myStruct* creater(int a, p_func func)
+
+
+
+/* simple creater func for struct*/
+myStruct* Creater(int a, p_func func)
 {
     myStruct *print_me = (myStruct*)malloc(sizeof(myStruct));
     assert(print_me);
@@ -37,22 +55,69 @@ myStruct* creater(int a, p_func func)
     return print_me;
 }
 
+/*super creater func for big struct*/
+mySuperStruct* SuperCreater(char arr[], p_tostrcmp funcCheck, p_func funcAct)
+{
+    mySuperStruct *bigStruct = (mySuperStruct*)malloc(sizeof(mySuperStruct));
+    
+    bigStruct -> command = arr;
+    bigStruct -> Checking = funcCheck;
+    bigStruct -> Action = funcAct;
+    
+    return bigStruct;
+}
 
 
-int main()
+
+
+
+
+
+
+int main(int argc, char *argv[], char *env[])
 {	
+	/*
 	int i;
 	myStruct *arrayOFstruct[10];
 	
 	for(i = 0; i < 10; ++i)
 	{
-		arrayOFstruct[i] = creater((i+1), Print);
+		arrayOFstruct[i] = Creater((i+1), Print);
 	}
 	
 	for(i = 0; i < 10; ++i)
 	{
 		arrayOFstruct[i] -> PrintMe(i);
 	}
+	*/
+	
+	char nameFile[50];
+	
+	p_tostrcmp* StrCmp;
+	mySuperStruct* SS;
+	
+	
+	(void)argc;
+	(void)env;
+	
+	printf("Insert the command: \n");
+	scanf("%s", &nameFile);
+	
+	
+	
+	SS = SuperCreater(nameFile, strcmp, Print);
+	
+	printf("%d",SS -> Checking(SS -> command, argv[1]));
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 	
 	
