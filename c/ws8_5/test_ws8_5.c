c723ad4c6c1c5e537e56a21a6362b7ff2bfedc91
ilia.rudnik <nov-gruz@mail.ru>
2022-08-24 17:29:31 +0300

    serial

---
+++ c/ws8_5/test_ws8_5.c
@@ -0,0 +1,22 @@
+/**********************
+* Title : Worksheet 8 
+* Author: Ilia Rudnik
+* Reviewer: 
+* Date : 28/08/2022
+***********************/
+#include<stdio.h>/* printf */
+#include <stdlib.h> /*free*/
+
+#include"ws8_5.h"
+
+
+int main()
+{
+
+
+
+
+
+
+return 0;
+}
---
+++ c/ws8_5/ws8_5.h
@@ -0,0 +1,7 @@
+#ifndef __WS8_5_H__
+#define __WS8_5_H__
+
+
+
+#endif
+
