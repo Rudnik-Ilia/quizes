2c308cfc66f9ec62958734e04a080e19a82fe395
ilia.rudnik <nov-gruz@mail.ru>
2022-08-24 09:55:11 +0300

    aii works try to add enum

--- c/ws8/test_ws8.c
+++ c/ws8/test_ws8.c
@@ -1,16 +1,16 @@
 #include<stdio.h>/* printf */
 #include<assert.h>/* assert */
 #include <stdlib.h> /*malloc*/
-
+#include<string.h> /* strlen*/
 
 #include"ws8.h"
 
-
 #define SIZE_NAME 10
+#define BUFF_FOR_3_CASE 50
 
 
 
-typedef int (*p_to_action)(const char*,const char*);
+
 
 typedef struct
 {
@@ -23,20 +23,27 @@ typedef struct
 SuperStruct* CreaterStruct(int nameOf ,void *value)
 {
     SuperStruct* BigStruct = (SuperStruct*)malloc(sizeof(SuperStruct));
-    
-    assert(BigStruct);
-    
+    assert(value); 
+    if(NULL == BigStruct)
+	{
+		puts("NO MEMORY FOR YOU!SORRY");
+	}
     BigStruct -> name = nameOf;
     BigStruct -> data = value;
-    
     return BigStruct;
 }
 
 void AddEveryThing(int num, SuperStruct* p_struc)
 {
+
 	int res;
 	float res2;
-	res = 0;
+	char *res3 =(char*)malloc(BUFF_FOR_3_CASE);
+	assert(p_struc);
+	if(NULL == res3)
+	{
+		puts("NO MEMORY FOR YOU!SORRY");
+	}
 	switch(p_struc -> name)
 	{
 		case 1:
@@ -45,6 +52,7 @@ void AddEveryThing(int num, SuperStruct* p_struc)
 			res += num;
 			*(int*)(p_struc -> data) = res;
 			break;
+			
 		case 2:
 		
 			res2 = *(float*)(p_struc -> data);
@@ -53,12 +61,31 @@ void AddEveryThing(int num, SuperStruct* p_struc)
 			break;
 			
 		case 3:
-			
-			
-		default:
-			printf("Error\n");
 		
-				
+			sprintf(res3, "%s%d", (char*)(p_struc -> data), num);
+			(p_struc -> data) = res3;					
+			
+	}
+}
+
+void PrintEverything(SuperStruct* p_struc)
+{
+	assert(p_struc);
+	
+	switch(p_struc -> name)
+	{
+		case 1:
+		
+			printf("%d", *(int*)(p_struc -> data));
+			break;
+		case 2:
+		
+			printf("%f", *(float*)(p_struc -> data));
+			break;
+		case 3:
+		
+			printf("%s\n", ((char*)(p_struc -> data)));
+			break;
 	}
 }
 
@@ -66,25 +93,35 @@ void AddEveryThing(int num, SuperStruct* p_struc)
 
 int main()
 {
+	
 	int x = 5;
 	float y = 3.14;
+	
 	
 	
 	SuperStruct* arrayOFstruct[3];
 	
 	arrayOFstruct[0] = CreaterStruct(1, &x);
 	arrayOFstruct[1] = CreaterStruct(2, &y);
-	arrayOFstruct[2] = CreaterStruct(3, "hello");
+	arrayOFstruct[2] = CreaterStruct(3, "Hello");
 	
 	/*
+	char string[BUFF_FOR_3_CASE] = "Hello";
 	printf("%s\n", arrayOFstruct[2] -> name);
 	printf("%c\n", *(char*)(arrayOFstruct[2] -> data));
 	*/
-	AddEveryThing(10, arrayOFstruct[1]);
+	
+	AddEveryThing(255, arrayOFstruct[2]);
+	/*
 	printf("%f\n", *(float*)(arrayOFstruct[1] -> data));
 	AddEveryThing(10, arrayOFstruct[1]);
-	printf("%f\n", *(float*)(arrayOFstruct[1] -> data));
-
+	printf("%f\n", *(float*)(arrayOFstruct[2] -> data));
+	for(i = 0; i < 10; ++i){
+		
+		printf("%c\n", ((char*)(arrayOFstruct[2] -> data))[i]);
+	}
+	*/
+	PrintEverything(arrayOFstruct[2]);
 	
 	
 	
--- c/ws8/ws8.h
+++ c/ws8/ws8.h
@@ -1,4 +1,8 @@
 #include<stdio.h>
 
 
-
+typedef enum {
+	INT = 1,
+	FLOAT = 2,
+	STR = 3
+} type;
