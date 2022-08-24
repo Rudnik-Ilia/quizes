b6596011d3220f3e91490b89bd3c58ec22abcb5c
ilia.rudnik <nov-gruz@mail.ru>
2022-08-24 11:26:09 +0300

    dooone

--- c/ws8/test_ws8.c
+++ c/ws8/test_ws8.c
@@ -1,91 +1,17 @@
-#include<stdio.h>/* printf */
-#include<assert.h>/* assert */
-#include <stdlib.h> /*malloc*/
-#include<string.h> /* strlen*/
+/**********************
+* Title : Worksheet 8 
+* Author: Ilia Rudnik
+* Reviewer: 
+* Date : 28/08/2022
+***********************/
 
+#include<stdio.h>/* printf */
+#include <stdlib.h> /*free*/
 #include"ws8.h"
 
-#define SIZE_NAME 10
-#define BUFF_FOR_3_CASE 50
+
 #define LEN_OF_ARR 3
 
-typedef struct
-{
-	type name;
-    void* data;
-    
-} SuperStruct;
-
-
-SuperStruct* CreaterStruct(type nameOf ,void *value)
-{
-    SuperStruct* BigStruct = (SuperStruct*)malloc(sizeof(SuperStruct));
-    assert(value); 
-    if(NULL == BigStruct)
-	{
-		puts("NO MEMORY FOR YOU!SORRY");
-	}
-    BigStruct -> name = nameOf;
-    BigStruct -> data = value;
-    return BigStruct;
-}
-
-void AddEveryThing(int num, SuperStruct* p_struc)
-{
-
-	int res;
-	float res2;
-	char* res3 = NULL;
-	assert(p_struc);
-	
-	switch(p_struc -> name)
-	{
-		case 1:
-			
-			res = *(int*)(p_struc -> data);
-			res += num;
-			*(int*)(p_struc -> data) = res;
-			break;
-			
-		case 2:
-		
-			res2 = *(float*)(p_struc -> data);
-			res2 += (float)num;
-			*(float*)(p_struc -> data) = res2;
-			break;
-			
-		case 3:
-		
-			res3 =(char*)malloc(BUFF_FOR_3_CASE);
-			if(NULL == res3)
-			{
-				puts("NO MEMORY FOR YOU!SORRY");
-			}
-			sprintf(res3, "%s%d", (char*)(p_struc -> data), num);
-			(p_struc -> data) = res3;	
-	}
-}
-
-void PrintEverything(SuperStruct* p_struc)
-{
-	assert(p_struc);
-	
-	switch(p_struc -> name)
-	{
-		case 1:
-		
-			printf("%d\n", *(int*)(p_struc -> data));
-			break;
-		case 2:
-		
-			printf("%0.2f\n", *(float*)(p_struc -> data));
-			break;
-		case 3:
-		
-			printf("%s\n", ((char*)(p_struc -> data)));
-			break;
-	}
-}
 
 
 
@@ -110,8 +36,6 @@ int main()
 	PrintEverything(arrayOFstruct[2]);
 	   
 	   
-	   
-	
 	free(arrayOFstruct[2] -> data);
 	for(i = 0; i < LEN_OF_ARR; ++i)
 	{
@@ -120,18 +44,6 @@ int main()
 	}
 	
 	
-	
-	
-
-
-
-
-
-
-
-
-
-
 
 return 0;
 }
--- c/ws8/ws8.h
+++ c/ws8/ws8.h
@@ -1,5 +1,5 @@
-#include<stdio.h>
-
+#ifndef __WS8_H__
+#define __WS8_H__
 
 typedef enum {
 	INT = 1,
@@ -8,3 +8,16 @@ typedef enum {
 } type;
 
 
+typedef struct
+{
+	type name;
+    void* data;
+    
+} SuperStruct;
+
+
+SuperStruct* CreaterStruct(type nameOf ,void *value);
+void AddEveryThing(int num, SuperStruct* p_struc);
+void PrintEverything(SuperStruct* p_struc);
+
+#endif
