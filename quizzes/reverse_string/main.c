38cc759001f29d4e003801b7453300f9efcf5e2b
ilia.rudnik <nov-gruz@mail.ru>
2022-08-27 13:13:33 +0300

    quizze reverse solved

--- quizzes/reverse_string/main.c
+++ quizzes/reverse_string/main.c
@@ -1,30 +1,47 @@
 #include<stdio.h>
 #include<string.h>
+#include <stdlib.h>
 
-#define LEN_STR 18
+
+#define LEN_STR 19
+#define LEN_REVERSE 22
 int main()
 {
 
 	int i;
 	int j;
-	char str[] = "I am ilia student!";
+	int count;
+	int record;
+	char str[] = " I am ilia student!";
+	char* second = (char*)malloc(LEN_REVERSE);
+	count = 0;
+	record = 0;
 	
 	
-	for(i = LEN_STR; i > 0; )
+	for(i = LEN_STR ; i > 0; )
 	{	
 		--i;	
-		
-		
-		if(str[i] == ' ')
-		{
-			
-			for(j = 0; j < LEN_STR - i -1 ; ++j)
+		if(str[i] == ' ' || i == 0)
+		{	
+			count-=1;
+			for(j = 0; j < LEN_STR - count; ++j)
 			{
-				printf("%c",str[i+j]);
+				/*
+				*/
+				printf("%c\n",str[i+j]);
+				
+				second[record++] = str[i+j];
+				
+	
+				++count;
 			}
 		}
 	}
-
+	
+	for(i = 0; i < LEN_REVERSE ; ++i)
+	{
+		printf("%c", second[i]);
+	}
 
 
 
