bb7cb83c04d9fa23cf23e863a624723eec95cf14
ilia.rudnik <nov-gruz@mail.ru>
2022-08-28 18:53:34 +0300

    2

--- c/ws9/test_ws9.c
+++ c/ws9/test_ws9.c
@@ -4,43 +4,58 @@
 
 #include"ws9.h"
 
-#define SIZE_ARRAY 80
+#define SIZE_ARRAY 12
 #define SIZE_W 8
 
-void Wordmemset(char *dest, int ch, size_t len) 
+void MMemSet(char *source, int c, size_t n) 
 {
 	size_t i;
     size_t word; 
     size_t loop; 
     size_t tail;
     size_t start;                  
-    unsigned char c = (unsigned char)ch; 
-    start = (size_t)dest % SIZE_W; 
+    unsigned char ch = (unsigned char)c; 
+    start = (size_t)source % SIZE_W; 
     
     for (i = 0, word = 0; i < SIZE_W  ; ++i) 
     {
-        word = word | ((size_t)c << i * SIZE_W );
+        word = word | ((size_t)ch << i * SIZE_W );
     }
     
     if(start != 0)
     {
     	for (i = 0; i < start; ++i)
     	{
-        *dest++ = c;
-        --len;
+        *source++ = ch;
+        --n;
         }
     }
 
-	loop = len / SIZE_W;
-	tail = len % SIZE_W;
+	loop = n / SIZE_W;
+	tail = n % SIZE_W;
 	
-    for (i = 0; i < loop ; ++i, dest += SIZE_W ) {
-        *(size_t*)dest = word;   
+    for (i = 0; i < loop ; ++i, source += SIZE_W ) {
+        *(size_t*)source = word;   
     }
-    for (i = 0; i < tail; ++i, ++dest) {
-        *dest = c;
+    for (i = 0; i < tail; ++i, ++source) {
+        *source = ch;
     }
 }
+
+void *MemCpy(void *dest, const void *src, size_t n)
+{
+	size_t i;
+	void* p;
+	char* ptr1 = dest;
+	const char* ptr2 = src; 
+	
+	for(i = 0; i < n; ++i)
+	{
+		*ptr1++ = *ptr2++;
+	}
+	return p;
+}
+
 
 
 int main()
@@ -49,12 +64,16 @@ int main()
 	
 	char *c;
 	int i;
+	char a[SIZE_ARRAY] = "i am ilia s";
 
     c = (char *) malloc(SIZE_ARRAY);
 	
-    
+    /*
     Wordmemset(c+1, 's', SIZE_ARRAY);
-
+	*/
+	
+	MemCpy(c, a, SIZE_ARRAY);
+	
     for (i = 0; i < SIZE_ARRAY ; ++i)
     {
         printf("%c", c[i]);
