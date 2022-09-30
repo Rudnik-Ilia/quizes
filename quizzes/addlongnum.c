d21c8331febbab4b096966ad4b9672f9072e53a0
ilia.rudnik <nov-gruz@mail.ru>
2022-09-30 11:45:40 +0300

    addlong

--- quizzes/addlongnum.c
+++ quizzes/addlongnum.c
@@ -5,27 +5,20 @@
 char *Add(char *num1, char *num2, char *output)
 {
 	int memo = 0;
-	int one = 0;
-	int two = 0;
 	char temp;
 	char sum;
-	size_t i = 15;
-	int len1 = 11;
+	int len1 = 10;
 	int len2 = 5;
-	int len3 = 20;
-	while(i)
+	int len3 = 16;
+	while(len2)
 	{	
-		one = num1[len1 - 1] - 48;
-		two = num2[len2 - 1] - 48;
-	
-		sum = one + two + memo;
+		sum = (num1[len1 - 1] - 48 ) + (num2[len2 - 1] - 48) + memo;
 		printf("sum: %d\n",sum);
 		temp = sum % 10 + 48;
 		printf("temp: %c\n",temp);
 		output[len3 - 1] = temp;
 		printf("mem: %d\n",memo);
 		printf("___________________________________\n");
-		--i;
 		--len1;
 		--len2;
 		--len3;
@@ -38,20 +31,26 @@ char *Add(char *num1, char *num2, char *output)
 		{
 			memo = 0;
 		}
-		
-		
+			
 	}
+	while(len1)
+	{
+		output[len3 - 1] = num1[len1 - 1];
+		--len3;
+		--len1;
+	}
+	
 	return output;	
 }
 
 int main()
 {
-	char num1[] = "12345678916";
-	char num2[] = "70771";
-	char output[20]; 
+	char num1[] = "7894418940";
+	char num2[] = "79770";
+	char output[16] = {0}; 
 	int i = 0;
 	Add(num1, num2, output);
-	for( ; i < 20; ++i)
+	for( ; i < 16; ++i)
 	{
 		printf("%c", output[i]);
 	}
