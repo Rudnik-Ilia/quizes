2e493ca0748499f9a465511187cfce2a4cfe893c
ilia.rudnik <nov-gruz@mail.ru>
2022-10-09 17:24:30 +0300

    next def

--- system_programming/VSA/src/vsa.c
+++ system_programming/VSA/src/vsa.c
@@ -13,6 +13,7 @@
 
 #include "vsa.h"
 
+#define SIZE_STR sizeof(block_t)
 
 typedef struct block_header{
 
@@ -26,7 +27,7 @@ typedef struct block_header{
 
 static size_t ResizeBlock(size_t block_size)
 {
-	return !(block_size % CHAR_BIT) ? block_size : (block_size - block_size % CHAR_BIT) + CHAR_BIT;
+	return !(block_size % CHAR_BIT) ? (long)block_size : (long)((block_size - block_size % CHAR_BIT) + CHAR_BIT);
 }
 
 
@@ -46,14 +47,55 @@ vsa_t *VSAInit(void *memory, size_t mem_size)
 	
 	block = (block_t *)((char *)memory + sizeof(block_t) + freespace);
 	block->size = 0;
-	
+	/*
 	printf("%ld\n", freespace);
-
+*/
 	return vsa;
 }
 
 
+void *VSAAlloc(vsa_t *vsa, size_t block_size)
+{
+	block_t *b = (block_t *)vsa;
+	size_t old_size = 0;
+	void *memory = NULL;
+	
+	block_size = ResizeBlock(block_size);
+	
+	while(0 != b->size)
+	{
+		if(0 == b->size)
+		{
+			return NULL;
+		} 
+		else if((long)block_size <= b->size)
+		{	
+			
+			memory = (char *)b + SIZE_STR;
+			
+			old_size = b->size;
+			
+			b->size = block_size * -1;
 
+			b = (block_t *)((char *)b + SIZE_STR + block_size);	
+			
+			b->size = old_size - block_size - SIZE_STR;
+
+			return memory;
+		}
+		else
+		{
+			if(b->size < 0)
+			{
+				b->size *= -1;
+			}
+			b = (block_t *)((char *)b + b->size + SIZE_STR);
+			
+		}
+	}
+	
+	return NULL;
+}
 
 
 
--- system_programming/VSA/test/test_vsa.c
+++ system_programming/VSA/test/test_vsa.c
@@ -10,11 +10,17 @@
 
 int main()
 {	
-	void *memory = (void *)malloc(100);
-	vsa_t *vsa = VSAInit(memory, 100);
+	void *memory = (void *)malloc(80);
+	vsa_t *vsa = VSAInit(memory, 80);
 	
 	
+	VSAAlloc(vsa,32);
+	VSAAlloc(vsa,16);
+	if(VSAAlloc(vsa,16) == NULL)
+	{
 	
+		puts("ERROR!");
+	}
 	
 
 	free(memory);
