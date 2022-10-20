5bcda55aa7a07d927b35317d5681bdf038912442
ilia.rudnik <nov-gruz@mail.ru>
2022-10-20 22:54:42 +0300

    benchmark

--- algorithms/src/sorts.c
+++ algorithms/src/sorts.c
@@ -1,11 +1,16 @@
 
 #include <stddef.h> /*size_t*/
+#include <assert.h> 
 #include "sorts.h"
 
 
-void Swap(int* x, int* y)
+void Swap(int *x, int *y)
 {
     int temp = *x;
+    
+    assert(NULL != x);
+    assert(NULL != y);
+    
     *x = *y;
     *y = temp;
 }
@@ -13,14 +18,13 @@ void Swap(int* x, int* y)
 void InsertionSort(int *arr, size_t size)
 {	size_t i = 0;
 	size_t j = 0;
-	int tmp = 0;
-	for(i=1; i < size; i++){
-		for(j=i; j>0 && arr[j-1]>arr[j]; j--)
+	
+	assert(NULL != arr);
+	
+	for(i = 1; i < size; ++i){
+		for(j = i; j > 0 && arr[j-1] > arr[j]; --j)
 		{
-
-			tmp=arr[j-1];
-			arr[j-1]=arr[j];
-			arr[j]=tmp;
+			 Swap(&arr[j], &arr[j-1]);
 		}
 	}
 }
@@ -29,9 +33,12 @@ void BubbleSort(int *arr, size_t size)
 {
 	size_t i = 0;
 	size_t j = 0;
+	
+	assert(NULL != arr);
+	
     	for (i = 0; i < size - 1; i++)
     	{
-		for (j = 0; j < size - i - 1; j++)
+		for (j = 0; j < size - i - 1; ++j)
 		{
 		    if (arr[j] > arr[j + 1])
 		    {
@@ -45,10 +52,43 @@ void BubbleSort(int *arr, size_t size)
 
 void SelectionSort(int *arr, size_t size)
 {
-
+	size_t i = 0;
+	size_t j = 0;
+	
+	assert(NULL != arr);
+	
+	for(i = 0; i < size; ++i)
+	{
+		for(j = i + 1; j < size; ++j)
+		{
+			if(arr[i] > arr[j])
+			{
+				Swap(&arr[j], &arr[i]);
+			}
+		}
+	}
 
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
--- algorithms/test/test_sorts.c
+++ algorithms/test/test_sorts.c
@@ -2,9 +2,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+
 #include "sorts.h"
 #include "test.h"
 
+
+#define SIZE_ARRAY 5000
 
 int isSorted(int *arr, size_t size)
 {
@@ -20,46 +23,65 @@ int isSorted(int *arr, size_t size)
     return 1;
 }
 
+int Comparator(const void * p1, const void * p2)
+{
+  return (*(int*)p1 - *(int*)p2);
+}
+
 void FillArray(int *arr, size_t size)
 {
 	size_t i = 0;
 	
-	for(i = 0; i < 5000; ++i)
+	for(i = 0; i < size; ++i)
 	{
-		arr[i] = rand()%3000;
+		arr[i] = rand() % size;
 	}
 }
 
 void MesureTime(int *arr, size_t size, void (*func)(int *arr, size_t size))
 {
 	
-	double total_t;
-	clock_t start_t;
-	clock_t end_t;
+	double total_t = 0;
+	clock_t end_t = 0;
+	clock_t start_t = clock();
 	
 	start_t = clock();
 	
 	func(arr, size);
+	  
+	end_t = clock();  
 	
-	end_t = clock();
-	   
 	total_t = (double)(end_t - start_t) / CLOCKS_PER_SEC;
+	
 	printf("Total time taken by CPU: %f\n", total_t  );
 	
 	TEST("Test for Insert" , isSorted(arr, size), 1);
-
 }
 
 int main()
 {
-	int arr[5000] = {0};
-	int size = 5000;
+	int arr[SIZE_ARRAY] = {0};
+	
+	FillArray(arr, SIZE_ARRAY);
+	
+	MesureTime(arr, SIZE_ARRAY, BubbleSort);
+	
+	FillArray(arr, SIZE_ARRAY);
+	
+	MesureTime(arr, SIZE_ARRAY, InsertionSort);
+	
+	FillArray(arr, SIZE_ARRAY);
+	
+	MesureTime(arr, SIZE_ARRAY, SelectionSort);
+	
+	FillArray(arr, SIZE_ARRAY);
+	
+	qsort(arr, SIZE_ARRAY, 4, Comparator);
+	
+	TEST("Test for Insert" , isSorted(arr, SIZE_ARRAY), 1);
 	
 	
-	FillArray(arr, size);
 	
-	MesureTime(arr, size, BubbleSort);
-	MesureTime(arr, size, InsertionSort);
 	
 
 	PASS;
