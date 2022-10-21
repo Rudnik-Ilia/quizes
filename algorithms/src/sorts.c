f3995d38db413b8f4d59441d5aebb304f61464f0
ilia.rudnik <nov-gruz@mail.ru>
2022-10-21 17:50:21 +0300

    count--

--- algorithms/src/sorts.c
+++ algorithms/src/sorts.c
@@ -1,18 +1,22 @@
 
 #include <stddef.h> /*size_t*/
 #include <assert.h> 
+#include <string.h> /*memset*/
+#include <stdlib.h> /*malloc*/
+
 #include "sorts.h"
 
 
 void Swap(int *x, int *y)
 {
-    int temp = *x;
+    int tmp = 0;
     
     assert(NULL != x);
     assert(NULL != y);
     
+    tmp = *x;
     *x = *y;
-    *y = temp;
+    *y = tmp;
 }
 
 void InsertionSort(int *arr, size_t size)
@@ -46,7 +50,6 @@ void BubbleSort(int *arr, size_t size)
 	            }
 		}        
 	}
-
 }
 
 
@@ -67,11 +70,78 @@ void SelectionSort(int *arr, size_t size)
 			}
 		}
 	}
-
-
 }
 
+void CountingSort(int *arr, size_t size)
+{
+	size_t i = 0; 
+	size_t size_of_tmp = 0;
+	int min = 0;
+	int max = size; 
+	int *tmp_arr = NULL; 
+	int *res_arr = NULL;
+	int xn; 
+	
+	assert(NULL != arr);
+ 
 
+	min = max = arr[0];
+	
+	for(i = 1; i < size ; ++i) 
+	{
+		if (arr[i] < min)
+		{
+			min = arr[i];
+		}
+		if (arr[i] > max)
+		{
+			max = arr[i];
+		}
+	}
+
+	size_of_tmp  = max - min + 1;
+
+	tmp_arr = (int*)malloc(size_of_tmp * sizeof(int));
+	res_arr = (int*)malloc(size * sizeof(int));
+	
+	memset(tmp_arr, 0, size_of_tmp);
+	memset(res_arr, 0, size);
+	
+	
+
+	
+	
+	for(i = 0; i < size; i++)
+	{
+		tmp_arr[arr[i] - min]++;
+	}
+	
+	
+/*	
+
+	for(i = 1; i < size_of_temp; ++i)
+	{
+		tmp_arr[i] = tmp_arr[i] + tmp_arr[i-1];
+	}
+
+	for(i = 0; i < n; i++)
+	{
+		res_arr[tmp_arr[arr[i] - min] - 1] = arr[i];
+		tmp_arr[arr[i] - min]--;
+	}
+
+
+	
+	for(i = 0; i < size; i++)
+	{ 
+		arr[i] = res_arr[i];
+	}
+
+*/
+	free(tmp_arr);
+	free(res_arr);
+
+}
 
 
 
--- algorithms/test/test_sorts.c
+++ algorithms/test/test_sorts.c
@@ -8,10 +8,12 @@
 
 
 #define SIZE_ARRAY 5000
+#define RANGE 100
 
 int isSorted(int *arr, size_t size)
 {
 	size_t i = 0;
+	
     	for (i = 0; i < size - 1; i++)
     	{
         	if (arr[i] > arr[i + 1]) 
@@ -28,19 +30,18 @@ int Comparator(const void * p1, const void * p2)
   return (*(int*)p1 - *(int*)p2);
 }
 
-void FillArray(int *arr, size_t size)
+void FillArray(int *arr, size_t size, size_t range)
 {
 	size_t i = 0;
 	
 	for(i = 0; i < size; ++i)
 	{
-		arr[i] = rand() % size;
+		arr[i] = rand() % range;
 	}
 }
 
 void MesureTime(int *arr, size_t size, void (*func)(int *arr, size_t size))
 {
-	
 	double total_t = 0;
 	clock_t end_t = 0;
 	clock_t start_t = clock();
@@ -53,37 +54,34 @@ void MesureTime(int *arr, size_t size, void (*func)(int *arr, size_t size))
 	
 	total_t = (double)(end_t - start_t) / CLOCKS_PER_SEC;
 	
-	printf("Total time taken by CPU: %f\n", total_t  );
+	printf("Total time taken by CPU: %f\n", total_t);
 	
-	TEST("Test for Insert" , isSorted(arr, size), 1);
 }
 
 int main()
 {
 	int arr[SIZE_ARRAY] = {0};
 	
-	FillArray(arr, SIZE_ARRAY);
-	
+	FillArray(arr, SIZE_ARRAY, 3000);
 	MesureTime(arr, SIZE_ARRAY, BubbleSort);
+	TEST("Test for Bubble" , isSorted(arr, SIZE_ARRAY), 1);
 	
-	FillArray(arr, SIZE_ARRAY);
-	
+	FillArray(arr, SIZE_ARRAY, 3000);
 	MesureTime(arr, SIZE_ARRAY, InsertionSort);
-	
-	FillArray(arr, SIZE_ARRAY);
-	
-	MesureTime(arr, SIZE_ARRAY, SelectionSort);
-	
-	FillArray(arr, SIZE_ARRAY);
-	
-	qsort(arr, SIZE_ARRAY, 4, Comparator);
-	
 	TEST("Test for Insert" , isSorted(arr, SIZE_ARRAY), 1);
 	
+	FillArray(arr, SIZE_ARRAY, 3000);
+	MesureTime(arr, SIZE_ARRAY, SelectionSort);
+	TEST("Test for Select" , isSorted(arr, SIZE_ARRAY), 1);
+	
+	FillArray(arr, SIZE_ARRAY, 3000);
+	qsort(arr, SIZE_ARRAY, 4, Comparator);
+	TEST("Test for Qsort" , isSorted(arr, SIZE_ARRAY), 1);
 	
 	
+	FillArray(arr, SIZE_ARRAY, 100);
+	CountingSort(arr, SIZE_ARRAY);
 	
-
 	PASS;
 return 0;
 }
