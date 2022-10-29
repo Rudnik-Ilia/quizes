c5b2949a15e7f14c8b691b13c2164e5d2735329c
ilia.rudnik <nov-gruz@mail.ru>
2022-10-29 11:58:34 +0300

    crazy array

--- ds/Calculator/calc.h
+++ ds/Calculator/calc.h
@@ -4,7 +4,7 @@
 #include "../include/stack.h"
 
 
-typedef void (*func)(stack_t * op_d, stack_t * op_or, void *data, char *ptr);
+typedef int (*func)(stack_t * op_d, stack_t * op_or, void *data, char *ptr);
 /*
 typedef enum status status_t
 
