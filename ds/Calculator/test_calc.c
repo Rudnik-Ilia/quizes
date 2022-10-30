042c55744ac38c93ca0a7ede0b47e7b5c3b09adf
ilia.rudnik <nov-gruz@mail.ru>
2022-10-30 16:38:01 +0200

    power

--- ds/Calculator/test_calc.c
+++ ds/Calculator/test_calc.c
@@ -6,11 +6,12 @@
 int main()
 {	
 	double *res = NULL;
+	/*
 	InfixToPost("2+1 ",res ,4);
 	InfixToPost("2-1 ",res ,4);
 	InfixToPost("9*8 ",res ,4);
 	InfixToPost("9/8 ",res ,4);
-	
+	*/
 	InfixToPost("3^3 ",res , 4);
 	
 
