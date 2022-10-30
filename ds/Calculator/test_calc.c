1b0089f7fdb62a042cba022b76177396907420c6
ilia.rudnik <nov-gruz@mail.ru>
2022-10-30 17:51:40 +0200

    equal

--- ds/Calculator/test_calc.c
+++ ds/Calculator/test_calc.c
@@ -7,12 +7,13 @@ int main()
 {	
 	double *res = NULL;
 	/*
-	InfixToPost("2+1 ",res ,4);
-	InfixToPost("2-1 ",res ,4);
-	InfixToPost("9*8 ",res ,4);
-	InfixToPost("9/8 ",res ,4);
 	*/
-	InfixToPost("3^3 ",res , 4);
+	InfixToPost("2+2=",res ,4);
+	InfixToPost("9*8=",res ,4);
+	InfixToPost("3+3=",res ,4);
+	InfixToPost("9/8=",res ,4);
+	InfixToPost("2-1=",res ,4);
+	InfixToPost("2^6=",res ,4);
 	
 
 	
