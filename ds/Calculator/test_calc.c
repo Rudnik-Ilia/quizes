41201bd8cdc76fa132e9a4486e9e06a917641393
ilia.rudnik <nov-gruz@mail.ru>
2022-11-02 13:52:43 +0200

    lastt

--- ds/Calculator/test_calc.c
+++ ds/Calculator/test_calc.c
@@ -11,12 +11,13 @@ int main()
 	/*
 	InfixToPost("9*8=",res ,4);
 	InfixToPost("9/8=",res ,4);
-	InfixToPost("2+6+3+7+6+8+5+3+7+8+9+6+9+2=",res ,25);
-	InfixToPost("(5*2)/2+(2+2)*2+3*(9-2) =",res ,30);
 	InfixToPost("20*3+(12+71)*2+45+1=",res ,5);
+	InfixToPost("2+6+3+7+6+8+5+3+7+8+9+6+9+2=",res ,25);
+	InfixToPost("((5*2)*2+(2+2)*2+3*(9-2))/2 =",res ,30);
+	InfixToPost("(4+4)*(2-5)*2=",res ,20);
+	InfixToPost("((2-5)*2)/3=",res ,20);
 	*/
-	
-	InfixToPost("-20 + 4=",res ,8);
+	InfixToPost("((4+3)+(2+4))*3=",res ,20);
 	
 	
 	
