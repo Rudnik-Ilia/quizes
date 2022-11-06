decd8452218b9407ee7edf10710badc9c7545b58
ilia.rudnik <nov-gruz@mail.ru>
2022-11-06 18:05:53 +0200

    secc+test

--- algorithms/src/recursion_2.c
+++ algorithms/src/recursion_2.c
@@ -3,11 +3,55 @@
 #include <sys/types.h> /* ssize_t */
 
 
+static ssize_t Bisearch(int *arr, int value_to_find, int min, int max)
+{
+	int middle = ( min + max ) / 2;  
+	if (max < min)
+	{
+		return -1;
+	}
+	if (arr[middle] > value_to_find)
+	{
+	 	return Bisearch(arr, value_to_find, min, middle - 1);
+	}	 
+	if (arr[middle] < value_to_find)
+	{
+		return Bisearch(arr, value_to_find, middle + 1, max);
+	}
+	if (value_to_find == arr[middle])
+	{
+		return middle;
+	}
+	return -1;
+}
 
 ssize_t BinarySearchIter(int *arr, size_t size, int value_to_find)
 {
-
-
+	int min = 0;
+	int max = size-1;
+	int middle = 0;
+	
+	while(1)
+	{	
+		middle = (min + max)/2;
+		
+		if(value_to_find > arr[middle])
+		{
+			min = middle + 1;
+		}
+		else if(value_to_find < arr[middle])
+		{
+			max = middle - 1;
+		}
+		else 
+		{
+			return middle;
+		}
+		if(min > max)
+		{
+			return -1;
+		}
+	}
 
 }
 
@@ -15,6 +59,6 @@ ssize_t BinarySearchIter(int *arr, size_t size, int value_to_find)
 ssize_t BinarySearchRec(int *arr, size_t size, int value_to_find)
 {
 
-
-
+	return Bisearch(arr, value_to_find, 0, size);
 }
+
--- algorithms/test/test_recursion_2.c
+++ algorithms/test/test_recursion_2.c
@@ -1,12 +1,58 @@
 #include <stdio.h>
 
 #include "recursion.h"
-
-
-int main()
+#include "test.h"
+/*
+ssize_t bisearch(int *arr, int value_to_find, int min, int max)
 {
-
-
-
+	int midpoint = ( min + max ) / 2;  
+	if (max < min)
+	{
+		return -1;
+	}
+	if (arr[midpoint] > value_to_find)
+	{
+	 	return bisearch(arr, value_to_find, min, midpoint - 1);
+	}	 
+	if (arr[midpoint] < value_to_find)
+	{
+		return bisearch(arr, value_to_find, midpoint + 1, max);
+	}
+	if (value_to_find == arr[midpoint])
+	{
+		return midpoint;
+	}
+	return -1;
+}
+*/
+int main()
+{	
+	int arr[] = {1,3,5,7,9,10,26,37,47,58,68,90,100, 104, 300, 333};
+	
+	size_t size = sizeof(arr)/sizeof(int);
+	
+	TEST("Iter", BinarySearchIter(arr,size, 68), 10);
+	TEST("Iter", BinarySearchIter(arr,size, 1), 0);
+	TEST("Iter", BinarySearchIter(arr,size, 333), 15);
+	TEST("Iter", BinarySearchIter(arr,size, 7), 3);
+	TEST("Iter", BinarySearchIter(arr,size, 90), 11);
+	TEST("Iter", BinarySearchIter(arr,size, 11111), -1);
+	TEST("Iter", BinarySearchIter(arr,size, 2), -1);
+	
+	
+	TEST("Recur", BinarySearchRec(arr, size, 68), 10);
+	TEST("Recur", BinarySearchRec(arr, size, 1), 0);
+	TEST("Recur", BinarySearchRec(arr, size, 333), 15);
+	TEST("Recur", BinarySearchRec(arr, size, 7), 3);
+	TEST("Recur", BinarySearchRec(arr, size, 90), 11);
+	TEST("Recur", BinarySearchRec(arr, size, 11111), -1);
+	TEST("Recur", BinarySearchRec(arr, size, 2), -1);
+	
+	
+	
+	
+	/*
+*/
+PASS;
 return 0;
 }
