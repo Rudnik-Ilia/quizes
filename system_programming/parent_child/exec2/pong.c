bc28872921399eedf0986c456022d32ab6a3ebd3
ilia.rudnik <nov-gruz@mail.ru>
2022-11-29 21:01:04 +0200

    second task pp

---
+++ system_programming/parent_child/exec2/pong.c
@@ -0,0 +1,36 @@
+#define _POSIX_C_SOURCE 1
+#include <stdio.h>
+#include <signal.h>  
+#include <unistd.h>
+
+void Handler_1();
+
+
+int main()
+{
+	struct sigaction user1;
+
+	user1.sa_handler = Handler_1;
+	user1.sa_flags = 0;
+	sigemptyset(&user1.sa_mask);
+
+	sigaction(SIGUSR1, &user1, NULL);
+
+	while(1)
+	{	
+		if(kill(getppid(), SIGUSR2) != 0)
+		{
+			return 1;
+		}
+		pause();
+		sleep(1);
+	}
+
+	return 0;
+}
+
+void Handler_1()
+{
+	puts("PING\n");
+	
+}
