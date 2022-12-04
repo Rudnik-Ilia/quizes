d5ffbd2ad0ffeb30f662629cfe399badb265738a
ilia.rudnik <nov-gruz@mail.ru>
2022-12-04 15:28:52 +0200

    100k first

---
+++ system_programming/threads/thread_1.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <pthread.h>
+#include <unistd.h>
+
+
+#define NUMBEROFTHREADS (100000)
+
+int ARRAY[NUMBEROFTHREADS] = {0};
+
+void* Stub(void *idx)
+{
+    int num = *(int*)idx;
+    ARRAY[num] = num;
+
+    return NULL;
+}
+
+int main()
+{
+    time_t start = 0;  
+    time_t final = 0;
+    size_t i = 0;
+    int arr_2[NUMBEROFTHREADS] = {0};
+    pthread_t arr_of_threads[NUMBEROFTHREADS];
+
+    time(&start);
+    
+    for(; i < NUMBEROFTHREADS; ++i)
+    {
+        arr_2[i] = i;
+        pthread_create(arr_of_threads + i, NULL, Stub, (void*)&arr_2[i]);
+    }
+
+    
+    time(&final);
+    
+    sleep(2);
+    
+   
+
+    for(i = 0; i < NUMBEROFTHREADS; ++i)
+    {
+        if((int)i != ARRAY[i])
+        {
+            break;
+        }
+        printf("%d\n", ARRAY[i]);
+          
+      
+    }
+
+    printf("Time of execution: %ld\n", final - start);
+
+
+    return 0;
+}
