c21e244f2ae3295c5e2073e46a5e02fe8b522c43
ilia.rudnik <nov-gruz@mail.ru>
2022-12-05 08:40:00 +0200

    four task thread

--- system_programming/threads/thread_3.c
+++ system_programming/threads/thread_3.c
@@ -20,16 +20,20 @@ int main()
     time_t start = 0;  
     time_t final = 0;
     size_t i = 0;
+    int res = 0;
     int arr_2[NUMBEROFTHREADS] = {0};
     pthread_t arr_of_threads[NUMBEROFTHREADS];
-    int res = 0;
+    pthread_attr_t deatach;
+    pthread_attr_init(&deatach);
+    pthread_attr_setdetachstate(&deatach, PTHREAD_CREATE_DETACHED);
+
 
     time(&start);
     
     for(; i < NUMBEROFTHREADS; ++i)
     {
         arr_2[i] = i;
-        res = pthread_create(arr_of_threads + i, NULL, Stub, (void*)&arr_2[i]);
+        res = pthread_create(arr_of_threads + i, &deatach, Stub, (void*)&arr_2[i]);
         if(0 != res)
         {
             while(0 != res)
@@ -57,6 +61,7 @@ int main()
 
     printf("Time of execution: %ld\n", final - start);
 
+    pthread_attr_destroy(&deatach);
 
     return 0;
 }
---
+++ system_programming/threads/thread_4.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <pthread.h>/*pthread*/
+#include <unistd.h>
+
+#define NUMBEROFTHREADS (31000)
+#define NUMBER (1000000000)
+#define RANGE NUMBER / NUMBEROFTHREADS
+
+void *SumDivisors(void *idx)
+{
+    size_t i = *(size_t*)&idx;
+    size_t sum = 0;
+    size_t end = i + RANGE;
+
+    if (end > NUMBER)
+    {
+        end = NUMBER;
+    }
+    
+    for( ; i <= end; ++i)
+    {
+        if(0 == NUMBER % i)
+        {
+            sum += i;
+        } 
+    }
+    return (*(void **)&sum);
+}
+
+int main()
+{
+    pthread_t arr_of_threads[NUMBEROFTHREADS] = {0};
+    size_t i = 0;
+    size_t sum = 0;
+    void *data = NULL;
+
+    for (i= 0; i < NUMBEROFTHREADS; ++i)
+    {
+        pthread_create(arr_of_threads + i, NULL, SumDivisors, (void *)((i * RANGE) + 1));
+    }
+    sleep(1);
+    for (i = 0; i < NUMBEROFTHREADS; ++i)
+    {
+        pthread_join(arr_of_threads[i], &data);
+        sum += (size_t)data;
+
+    } 
+ 
+    printf("SUMM: %lu\n", sum);
+
+    return 0;
+}
