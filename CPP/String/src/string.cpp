19b74092723eeca5860c99e3c8704c3011caf4eb
ilia.rudnik <nov-gruz@mail.ru>
2023-01-16 11:11:30 +0200

    after review

--- CPP/String/src/string.cpp
+++ CPP/String/src/string.cpp
@@ -1,39 +1,85 @@
-/*****************************************************
+/****************************************************
 * Topic: String                                               
 * Author: Rudnik Ilia                                
 * Date: 15.01.2023                                
 * Reviewer: Tim                         
 * Review status: reviewed                            
 *****************************************************/
+
 #include <cstdlib>
 #include <cstring> 
 #include "string.hpp"
 
 using namespace ilrd;
 
-String::String(const char *str): m_str(strdup(str)) 
-{}
+String::String(const char *str): m_str(new char[strlen(str) + 1]) 
+{
+    memcpy(m_str, str, strlen(str) + 1);  
+}
 
-String::String(const String& str2): m_str(strdup(str2.Cstr())) 
-{}
+String::String(const String&  other): m_str(new char[other.Length()+1])
+{
+     memcpy(m_str, other.m_str, other.Length() + 1);
+}
 
 String::~String()
 {
-    free(m_str);
+    delete[] m_str;
     m_str = NULL;
 }
 
-String& String::operator=(const String& other) 
-{
-    free(m_str);
-    m_str = strdup(other.m_str);
-    return *this;
-}
 size_t String::Length() const
 {
     return strlen(m_str);
 }
-const char *String::Cstr() const 
+
+const char* String::Cstr() const 
 {
-    return m_str;
+    return this->m_str;
 }
+
+String &String::operator=(const String &other)
+{
+    delete[] m_str;
+    m_str = new char[strlen(other.m_str) + 1];
+    memcpy(m_str, other.m_str, strlen(other.m_str) + 1);
+    return *this;
+}
+// SECOND VRIANT---------------------------------------------------------------------------------
+
+// String::String(const char *str): m_str(strdup(str)) 
+// {
+//     if(m_str == NULL)
+//     {
+//         return;
+//     }
+// }
+
+// String::String(const String& str2): m_str(strdup(str2.Cstr())) 
+// {
+//      if(m_str == NULL)
+//     {
+//         return;
+//     }
+// }
+
+// String::~String()
+// {
+//     free(m_str);
+//     m_str = NULL;
+// }
+
+// String& String::operator=(const String& other) 
+// {
+//     free(m_str);
+//     m_str = strdup(other.m_str);
+//     return *this;
+// }
+// size_t String::Length() const
+// {
+//     return strlen(m_str);
+// }
+// const char *String::Cstr() const 
+// {
+//     return m_str;
+// }
--- CPP/String/test/test_string.cpp
+++ CPP/String/test/test_string.cpp
@@ -10,19 +10,14 @@ using namespace ilrd;
 int main()
 {
     
-    
-    char *str = "Rudnik";
-    String s1(str);
+    // char *str = "Rudnik";
+    String s1("Rudnik");
     String s2(s1);
 
-    TEST("len",  strlen(str), s1.Length());
-    TEST("len",  strlen(str), s2.Length());
+    TEST("len",  strlen("Rudnik"), s1.Length());
+    TEST("len",  strlen("Rudnik"), s2.Length());
     TEST("len",  strcmp(s1.Cstr(), s2.Cstr()), 0);
  
-
-
-
-
     PASS;
     return 0;
 }
