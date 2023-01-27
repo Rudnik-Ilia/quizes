9fd1206a81a9926915e50f6cf78ba5a2b1ad6507
ilia.rudnik <nov-gruz@mail.ru>
2023-01-27 18:01:25 +0200

    print

--- CPP/Containers/test/test_histo.cpp
+++ CPP/Containers/test/test_histo.cpp
@@ -11,19 +11,18 @@ using namespace std;
 int main()
 {
     int i = 0;
-    int count = 1;
+    string str;
     map <string, int> data;
     vector<string> arr; 
     
-    while(i < 6)
+    while(str != ".")
     {
-        string str;
         cout << "Insert string: ";
         cin >> str;
 
         if (data.find(str) == data.end())
         {
-            data.insert(pair<string, int>(str, count));
+            data.insert(pair<string, int>(str, 1));
             arr.push_back(str);
         }
         else
@@ -34,17 +33,27 @@ int main()
         ++i;
     }
    
-    map<string, int>:: iterator iter = data.begin();
-    for(std::size_t k = 0; k < data.size(); ++k)
-    {
-        cout << iter->first << ' ' << iter->second << endl;
-        ++iter;
-    }
-    cout << "------------------" << endl;
+    // map<string, int>:: iterator iter = data.begin();
+    // for(std::size_t k = 0; k < data.size(); ++k)
+    // {
+    //     cout << iter->first << ' ' << iter->second << endl;
+    //     ++iter;
+    // }
+    // cout << "------------------" << endl;
 
-     for(std::size_t k = 0; k < arr.size(); ++k)
+    for(std::size_t k = 0; k < arr.size(); ++k)
     {
-        cout << arr[k] << endl;
+        int x = data[arr[k]];
+        if(x == 1)
+        {
+            cout << ((x == 1)? " " : " x " ) << arr[k] << endl;
+        }
+        else
+        {
+            cout << x << ((x == 1)? " " : " x " ) << arr[k] << endl;
+        }
+
+        // cout << ((x == 1)? ' ': x ) << ((x == 1)? " " : " x " ) << arr[k] << endl;
     }
 
 
