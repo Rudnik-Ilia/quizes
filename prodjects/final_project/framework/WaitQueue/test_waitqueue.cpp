68c1c4ea37b1b613574389da4fff836c1574b827
ilia.rudnik <nov-gruz@mail.ru>
2023-03-01 18:00:36 +0200

    +defence from wakeups

---
+++ prodjects/final_project/framework/WaitQueue/test_waitqueue.cpp
@@ -0,0 +1,80 @@
+#include <iostream>
+using std::cout;
+using std::endl;
+#include <thread>
+using std::thread;
+#include <unistd.h>
+
+#include "waitqueue.hpp"
+
+
+#define COLOR_RED "\033[1;31m" 
+#define COLOR_BLUE "\033[1;34m" 
+#define OFFCOLOR "\033[0m" 
+
+using namespace ilrd;
+
+WaitableQueue<int> mq1;
+
+void Thread1()
+{
+    int x = 1;
+    while(1)
+    {
+        cout << "PUT()  " << x << endl;
+        mq1.Push(x++);
+        sleep(1);
+    }
+   
+}
+
+void Thread2()
+{
+    int tmp = 0;
+
+    while(1)
+    {
+        mq1.Pop(tmp);
+        cout << "TAKE(1)  " << tmp << endl;
+    }
+}
+
+void Thread2_1()
+{
+    int tmp = 0;
+
+    while(1)
+    {
+        mq1.Pop(tmp);
+        cout << "TAKE(2)  " << tmp << endl;
+    }
+}
+
+void Thread3()
+{
+    int tmp = 0;
+    std::chrono::milliseconds timeout(1000);
+
+    while(1)
+    {
+        mq1.Pop(timeout, tmp);
+        cout << "TAKE(3)  " << tmp << endl;
+    }  
+}
+
+int main()
+{
+    thread t1(&Thread1);
+    // thread t2(&Thread2);
+    // thread t2_1(&Thread2_1);
+
+    thread t3(&Thread3);
+
+    t1.join(); 
+    // t2.join(); 
+    // t3.join();
+
+
+    return 0;
+
+}
---
+++ prodjects/final_project/framework/WaitQueue/waitqueue.hpp
@@ -0,0 +1,92 @@
+#ifndef __ILRD_RD132_WAITABLEQUEUE_H__
+#define __ILRD_RD132_WAITABLEQUEUE_H__
+
+#include <iostream>
+#include <chrono>
+#include <condition_variable>
+#include <mutex>
+#include <queue>
+#include <chrono>
+
+namespace ilrd 
+{
+
+    template <typename T, typename CONTAINER = std::queue<T>> class WaitableQueue 
+    {
+    public:
+    explicit WaitableQueue() = default;
+    explicit WaitableQueue(const WaitableQueue &other_) = delete;
+    explicit WaitableQueue(WaitableQueue &&other_) = default;
+    WaitableQueue &operator=(const WaitableQueue &other_) = delete;
+    WaitableQueue &operator=(WaitableQueue &&other_) = default;
+    ~WaitableQueue() = default;
+
+    bool Pop(std::chrono::milliseconds time_ms_, T &outparam_);
+    void Pop(T &outparam_);
+    void Push(const T &element);
+    bool IsEmpty() const;
+
+    private:
+    CONTAINER m_container{};
+    mutable std::mutex m_mutex{};
+    std::condition_variable m_nonEmptyCond{};
+
+    bool fake_wakeup = false;
+
+    }; // class WQueue
+
+
+    template<typename T, typename CONTAINER> 
+    void WaitableQueue<T, CONTAINER>::Push(const T &element)
+    {
+        std::unique_lock<std::mutex> lock(m_mutex);
+        m_container.push(element);
+        lock.unlock();
+        m_nonEmptyCond.notify_one();
+        fake_wakeup = true;
+    }
+
+    template<typename T, typename CONTAINER> 
+    bool WaitableQueue<T, CONTAINER>::IsEmpty() const
+    {
+        std::unique_lock<std::mutex> lock(m_mutex);
+        return m_container.empty();
+    }
+
+    template<typename T, typename CONTAINER> 
+    void WaitableQueue<T, CONTAINER>::Pop(T &outparam_)
+    {
+        std::unique_lock<std::mutex> lock(m_mutex);
+        while(m_container.empty())
+        {
+            m_nonEmptyCond.wait(lock, [&]{return !this->fake_wakeup;});
+        }
+        outparam_ = m_container.front();
+        m_container.pop();
+        fake_wakeup = false;
+    }
+
+    template<typename T, typename CONTAINER> 
+    bool WaitableQueue<T, CONTAINER>::Pop(std::chrono::milliseconds time_ms_, T &outparam_)
+    {
+        std::unique_lock<std::mutex> lock(m_mutex);
+        std::chrono::time_point<std::chrono::system_clock> time_until = std::chrono::system_clock::now() + time_ms_;
+
+        while(m_container.empty())
+        {
+            if(std::cv_status::timeout == m_nonEmptyCond.wait_until(lock, time_until))
+            {
+                return false;
+            }
+        }
+
+        outparam_ = m_container.front();
+        m_container.pop();
+        fake_wakeup = false;
+
+        return true;
+    }
+
+} // namespace ilrd
+
+#endif /* __ILRD_RD132_WAITABLEQUEUE_H__ */
