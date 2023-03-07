662ebd95da7ccf4206f4af251b9b2e1653daf173
ilia.rudnik <nov-gruz@mail.ru>
2023-03-07 08:44:34 +0200

    test

--- prodjects/final_project/framework/test/test_workerthread.cpp
+++ prodjects/final_project/framework/test/test_workerthread.cpp
@@ -7,32 +7,63 @@ using std::endl;
 #include "WorkerThread.hpp"
 #include "ITask.hpp"
 #include "ThreadPool.hpp"
+
 using namespace ilrd;
 
 
-class MyTask: public ITask
+class MyTaskNormal: public ITask
 {
     public:
     void Execute()
     {
-            cout << "Thread Run" << endl;  
+   
+        cout << "Thread Run NORMAL" << endl;  
+        
+    }
+
+};
+class MyTaskHigh: public ITask
+{
+    public:
+    void Execute()
+    {
+    
+        cout << "Thread Run HIGH" << endl;  
+        
+    }
+
+};
+class MyTaskLow: public ITask
+{
+    public:
+    void Execute()
+    {
+        cout << "Thread Run LOW" << endl;  
+        
     }
 
 };
 
 std::shared_ptr<ITask> getTask()
 {
-    return std::shared_ptr<ITask>(new MyTask());
+    return std::shared_ptr<ITask>(new MyTaskNormal());
 }
 
 int main()
 {
+    std::shared_ptr<MyTaskNormal>(new MyTaskNormal());
     std::function<std::shared_ptr<ITask>()> send_func = std::bind(getTask);
 
-    // send_func().get()->Execute();
+    // WorkerThread *work = new WorkerThread(send_func);
 
-    WorkerThread *work = new WorkerThread(send_func);
-    sleep(1);
+    ThreadPool *pool = new ThreadPool(10);
+
+    pool->AddTask(std::shared_ptr<MyTaskNormal>(new MyTaskNormal()), ThreadPool::PRIORITY_NORMAL);
+    pool->AddTask(std::shared_ptr<MyTaskHigh>(new MyTaskHigh()), ThreadPool::PRIORITY_HIGH);
+    pool->AddTask(std::shared_ptr<MyTaskLow>(new MyTaskLow()), ThreadPool::PRIORITY_LOW);
+
+    sleep(3);
+    
 
     return 0;
 }
