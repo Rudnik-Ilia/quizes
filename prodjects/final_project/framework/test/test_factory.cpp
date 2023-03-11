335214b384e329ac354e4e4e8ce953806948de2d
ilia.rudnik <nov-gruz@mail.ru>
2023-03-11 17:41:39 +0200

    test for fact

--- prodjects/final_project/framework/test/test_factory.cpp
+++ prodjects/final_project/framework/test/test_factory.cpp
@@ -3,6 +3,7 @@ using std::cout;
 using std::endl;
 using std::string;
 
+// gp11 test_factory.cpp -I ../include (from  test)
 
 #include "Factory.hpp"
 #include "ITask.hpp"
@@ -36,6 +37,21 @@ class WriteFunc: public ITask
         size_t m_num;
 };
 
+class DeleteFunc: public ITask
+{
+    public:
+        DeleteFunc(int a, size_t b, double c): m_a(a), m_b(b), m_c(c){}
+        void Execute()
+        {
+            cout << m_a << '+' << m_b << '+' <<  m_c << ' ' << endl;
+        }
+
+    private:
+        int m_a;
+        size_t m_b;
+        double m_c;
+};
+
 
 struct ArgsForRead: public FactoryArgs
 {
@@ -50,6 +66,13 @@ struct ArgsForWrite: public FactoryArgs
 
     string &m_str;
     size_t m_num;
+};\
+
+struct ArgsForDelete: public FactoryArgs
+{
+    int m_a;
+    size_t m_b;
+    double m_c;
 };
 
 std::shared_ptr<ITask> CreaterRead(FactoryArgs &arguments)
@@ -64,6 +87,12 @@ std::shared_ptr<ITask> CreaterWrite(FactoryArgs &arguments)
     return std::shared_ptr<WriteFunc>(new WriteFunc(args.m_str,args.m_num));
 }
 
+std::shared_ptr<ITask> CreaterDelete(FactoryArgs &arguments)
+{
+    ArgsForDelete &args =  dynamic_cast<ArgsForDelete&>(arguments);
+    return std::shared_ptr<DeleteFunc>(new DeleteFunc(args.m_a ,args.m_b, args.m_c));
+}
+
 
 
 
@@ -71,20 +100,23 @@ int main()
 {   
     string str1 = "Im reader";
     string str2 = "Im writer";
-    
+
     ArgsForRead reader(str1);
     ArgsForWrite writer(str2, 555);
+    struct ArgsForDelete deleter;
+    deleter.m_a = 11;
+    deleter.m_b = 22;
+    deleter.m_c = 3.14;
 
     Factory<ITask, string> factor;
 
     factor.Register("R", CreaterRead);
     factor.Register("W", CreaterWrite);
-
+    factor.Register("D", CreaterDelete);
 
     factor.Create("R", reader)->Execute();
     factor.Create("W", writer)->Execute();
-
-
+    factor.Create("D", deleter)->Execute();
 
     return 0;
 }
